4e9e6a05769d8ba575fb2f5d71ef6c13f21e5045
Ben Xu <benx.xu@mail.utoronto.ca>
2024-07-31 10:41:45 -0400

    add token endpoint

--- interpreter/core/async_core.py
+++ interpreter/core/async_core.py
@@ -554,6 +554,23 @@ def create_router(async_interpreter):
 
         yield "data: [DONE]\n\n"
 
+    @router.get("/token")
+    def getToken():
+        token = (
+            api.AccessToken(
+                os.getenv("LIVEKIT_API_KEY"), os.getenv("LIVEKIT_API_SECRET")
+            )
+            .with_identity("identity")
+            .with_name("my name")
+            .with_grants(
+                api.VideoGrants(
+                    room_join=True,
+                    room="my-room",
+                )
+            )
+        )
+        return {"accessToken": token.to_jwt()}
+
     @router.post("/v0/chat/completions")
     async def chat_completion(request: ChatCompletionRequest):
         if not (request.messages and request.messages[-1].role == "user"):
@@ -768,6 +785,15 @@ def create_router(async_interpreter):
             }
 
     return router
+
+
+host = os.getenv(
+    "HOST", "0.0.0.0"
+)  # IP address for localhost, used for local testing. To expose to local network, use 0.0.0.0
+port = int(os.getenv("PORT", 8000))  # Default port is 8000
+
+# FOR TESTING ONLY
+host = "0.0.0.0"
 
 
 class Server:
